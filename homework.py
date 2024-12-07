82997e95961d7c5d0d01635e01a3420e6fd95be9
Rephael4321 <Rephael4321@gmail.com>
2024-12-08 00:39:47 +0200

    Updated homework - lesson 14

--- homework.py
+++ homework.py
@@ -8,20 +8,26 @@ def main(num):
     print(f"PID: {os.getpid()}")
     print(f"Parent PID: {os.getppid()}")
     print()
-    process = multiprocessing.Process(target=count, args=(num, ))
+    queue = multiprocessing.Queue()
+    process = multiprocessing.Process(target=count, args=(num, queue))
     process.start()
     print("Stater but not joined")
     process.join()
+    total = queue.get()
+    print(f"The total is {total}")
 
 
-def count(num):
+def count(num, queue):
     print("Sub process (count)")
     print(f"PID: {os.getpid()}")
     print(f"Parent PID: {os.getppid()}")
     print()
+    total = 0
     for x in range(num):
+        total += x
         print(x)
         time.sleep(1)
+    queue.put(total)
 
 
 if __name__ == "__main__":
